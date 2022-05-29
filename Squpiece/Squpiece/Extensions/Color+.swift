efd6a934115f93fad91e37d1219c6663cb1da0fb
jeonghyeon Hwang <hijihilin@gmail.com>
2022-05-29 22:50:58 +0900

    [FEAT] Add Color

---
+++ Squpiece/Squpiece/Extensions/Color+.swift
@@ -0,0 +1,20 @@
+//
+//  Color+.swift
+//  Squpiece
+//
+//  Created by 황정현 on 2022/05/28.
+//
+
+import SwiftUI
+
+// MARK: Color extension
+extension Color {
+    static let backgroundColor = Color("BackgroundColor")
+    static let pieceColor1 = Color("PieceColor1")
+    static let pieceColor2 = Color("PieceColor2")
+    static let pieceColor3 = Color("PieceColor3")
+    static let pieceColor4 = Color("PieceColor4")
+    
+    static let blackColor = Color("BlackColor")
+    static let shadowColor = Color("ShadowColor")
+}
