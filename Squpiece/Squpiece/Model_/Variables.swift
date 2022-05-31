0f1dc97c81203d559759d0ed6ab7b2a26a398c05
jeonghyeon Hwang <hijihilin@gmail.com>
2022-05-31 21:24:26 +0900

    [REFACTOR] Add Script for Variables (#7)

---
+++ Squpiece/Squpiece/Model_/Variables.swift
@@ -0,0 +1,19 @@
+//
+//  Variables.swift
+//  Squpiece
+//
+//  Created by 황정현 on 2022/05/31.
+//
+
+import Foundation
+import SwiftUI
+
+var change = false
+var touched = false
+
+var rotateAngle: CGFloat = 5
+var rotationStop =  false
+var randomStop = false
+
+var waitSec: Double = 1
+var degree: Double = 55
