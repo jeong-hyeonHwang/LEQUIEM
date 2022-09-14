f0a66a3061afd68c02586f53792a85a768365b4d
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-14 23:20:03 +0900

    [REFACTOR] Remove Useless Method
    - SoundFunction.swift -> backgroundMusicPlay

--- Squpiece/Squpiece/ViewModel/SoundFunction.swift
+++ Squpiece/Squpiece/ViewModel/SoundFunction.swift
@@ -7,19 +7,6 @@
 
 import SpriteKit
 
-func backgroundMusicPlay(node: SKAudioNode) {
-    let waitASec = SKAction.wait(forDuration: 1)
-    let play = SKAction.run {
-        node.run(SKAction.play())
-    }
-    let wait = SKAction.wait(forDuration: 65)
-    let sequence = SKAction.sequence([play, wait])
-    let repeatSequence = SKAction.repeatForever(sequence)
-    let lastResult = SKAction.sequence([waitASec, repeatSequence])
-    
-    node.run(lastResult)
-}
-
 func sfxPlay(soundFileName: String, scene: SKScene) {
     if(sfxBool == true) {
         let gotoGameSceneSFX = SKAction.playSoundFileNamed("\(soundFileName).mp3", waitForCompletion: false)
