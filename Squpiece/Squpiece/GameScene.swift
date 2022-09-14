1b0f95bb12642424f9e191e69d791ea3bb966996
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-14 23:44:50 +0900

    [REFACTOR] Add CGPath Extension File

--- Squpiece/Squpiece/GameScene.swift
+++ Squpiece/Squpiece/GameScene.swift
@@ -8,21 +8,6 @@
 import GameKit
 import SpriteKit
 
-extension CGPath {
-    static func arcWithWidth(center: CGPoint, start:CGFloat, end:CGFloat, radius:CGFloat, clockwise:Bool) -> UIBezierPath {
-        // The radius parameter specifies the middle of the arc; adjust this as needed
-
-        // Note the arc is upside down because CGPath uses UIKit coordinates
-        let path = UIBezierPath()
-        // Add inner ring.
-        path.addArc(withCenter: center, radius: radius, startAngle: start, endAngle: end, clockwise: true)
-        path.addLine(to: center)
-        path.close()
-
-        return path
-    }
-}
-
 final class GameScene: SKScene {
     
     // Presetting Values
